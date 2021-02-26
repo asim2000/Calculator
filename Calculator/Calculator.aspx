36877194fa7feac7a4bcf30665b1ebf55356ea5a
Asim <asimbabayev0002@gmail.com>
2021-02-26 21:09:36 +0400

    Add project files.

---
+++ Calculator/Calculator.aspx
@@ -0,0 +1,81 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Calculator.aspx.cs" Inherits="Calculator.Calculator" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+ <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"/>
+
+    <title></title>
+    <style>
+        table{
+           // background:repeating-linear-gradient(red,blue,green,black);
+           box-shadow: 10px 5px 5px gray;
+        }
+        
+    </style>
+</head>
+<body>
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-4">
+                <form runat="server">
+                    <asp:TextBox ID="num1" Visible="false" runat="server"></asp:TextBox>
+                    <asp:TextBox ID="num2" Visible="false" runat="server"></asp:TextBox>
+                    <table class="table table-bordered bg-dark">
+                       <thead>
+                           <tr>
+                               <th colspan="4"><asp:Label ID="label1" runat="server" CssClass="text-white" style="padding-left:200px;"></asp:Label> </th>
+                           </tr>
+                           <tr>
+                               <th colspan="4">
+                                   <asp:TextBox ID="textBox1" ReadOnly="true" CssClass="form-control text-right my-2 bg-muted" runat="server"></asp:TextBox>
+                               </th>
+                           </tr>
+                       </thead>
+                        <tbody>
+                            <tr>
+                                <td><asp:Button Text="C" runat="server" CssClass="btn btn-primary w-100" OnClick="Unnamed1_Click1" /></td>
+                                <td><asp:Button Text="<" runat="server" CssClass="btn btn-primary w-100" OnClick="Unnamed2_Click" /></td>
+                                <td><asp:Button Text="." runat="server" CssClass="btn btn-primary w-100" OnClick="Unnamed3_Click" /></td>
+                                <td><asp:Button Text="+" runat="server" CssClass="btn btn-primary w-100" OnClick="Unnamed4_Click" /></td>
+                            </tr>
+                            <tr>
+                                <td><asp:Button Text="1" runat="server" CssClass="btn btn-primary w-100" OnClick="Unnamed5_Click" /></td>
+                                <td><asp:Button Text="2" runat="server" CssClass="btn btn-primary w-100" OnClick="Unnamed6_Click" /></td>
+                                <td><asp:Button Text="3" runat="server" CssClass="btn btn-primary w-100" OnClick="Unnamed7_Click" /></td>
+                                <td><asp:Button Text="/" runat="server" CssClass="btn btn-primary w-100" OnClick="Unnamed8_Click" /></td>
+                            </tr>
+                            <tr>
+                                <td><asp:Button Text="4" runat="server" CssClass="btn btn-primary w-100" OnClick="Unnamed9_Click" /></td>
+                                <td><asp:Button Text="5" runat="server" CssClass="btn btn-primary w-100" OnClick="Unnamed10_Click" /></td>
+                                <td><asp:Button Text="6" runat="server" CssClass="btn btn-primary w-100" OnClick="Unnamed11_Click" /></td>
+                                <td><asp:Button Text="X" runat="server" CssClass="btn btn-primary w-100" OnClick="Unnamed12_Click" /></td>
+                            </tr>
+                            <tr>
+                                <td><asp:Button ID="yeddi" Text="7" runat="server" CssClass="btn btn-primary w-100" OnClick="Unnamed13_Click" /></td>
+                                <td><asp:Button Text="8" runat="server" CssClass="btn btn-primary w-100" OnClick="Unnamed14_Click" /></td>
+                                <td><asp:Button type="button" Text="9" runat="server" CssClass="btn btn-primary w-100" OnClick="Unnamed15_Click" /></td>
+                                <td><asp:Button type="button" Text="-" runat="server" CssClass="btn btn-primary w-100" OnClick="Unnamed16_Click" /></td>
+                            </tr>
+                            <tr>
+                                <td><asp:Button type="button" Text="+/-" runat="server" CssClass="btn btn-primary w-100" OnClick="Unnamed17_Click" /></td>
+                                <td><asp:Button type="button" Text="0" runat="server" CssClass="btn btn-primary w-100" OnClick="Unnamed18_Click" /></td>
+                                <td colspan="2"><asp:Button Text="=" runat="server" CssClass="btn btn-primary w-100" OnClick="Unnamed19_Click" /></td>
+                                </tr>
+                        </tbody>
+                    </table>
+                </form>
+            </div>
+            
+        </div>
+    </div>
+    <script>
+        //document.querySelector("table").addEventListener("click", (e) => {
+        //    e.preventDefault();
+        //});
+    </script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
---
+++ Calculator/Calculator.aspx.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Calculator
+{
+    public partial class Calculator : System.Web.UI.Page
+    {
+        double say1, say2, netice;
+        string op;
+        void method(string oprt)
+        {
+            if (textBox1.Text != "")
+            {
+
+                num2.Text = oprt;
+                num1.Text = textBox1.Text;
+                //say1 = Convert.ToDouble(textBox1.Text);
+                label1.Text = num1.Text + " " + oprt;
+                textBox1.Text = "";
+            }
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Unnamed1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Unnamed1_Click1(object sender, EventArgs e)
+        {
+            label1.Text = "";
+            textBox1.Text = "0";
+
+        }
+
+        protected void Unnamed2_Click(object sender, EventArgs e)
+        {
+            //label1.Text = "";
+            //textBox1.Text = "0";
+            if (textBox1.Text!="") {
+                textBox1.Text = textBox1.Text.Substring(0, textBox1.Text.Length - 1);
+            }
+        }
+
+        protected void Unnamed3_Click(object sender, EventArgs e)
+        {
+
+            if (textBox1.Text.IndexOf('.') < 1 && textBox1.Text != "")
+            {
+                textBox1.Text += ".";
+            }
+        }
+
+        protected void Unnamed4_Click(object sender, EventArgs e)
+        {
+            method("+");
+        }
+
+        protected void Unnamed16_Click(object sender, EventArgs e)
+        {
+            method("-");
+        }
+
+        protected void Unnamed5_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "1";
+            }
+
+            else
+            {
+                textBox1.Text += "1";
+            }
+        }
+
+        protected void Unnamed6_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "2";
+            }
+
+            else
+            {
+                textBox1.Text += "2";
+            }
+        }
+
+        protected void Unnamed7_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "3";
+            }
+
+            else
+            {
+                textBox1.Text += "3";
+            }
+        }
+
+        protected void Unnamed9_Click(object sender, EventArgs e)
+        {
+             
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "4";
+            }
+
+            else
+            {
+                textBox1.Text += "4";
+            }
+        }
+
+        protected void Unnamed10_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "5";
+            }
+
+            else
+            {
+                textBox1.Text += "5";
+            }
+        }
+
+        protected void Unnamed11_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "6";
+            }
+
+            else
+            {
+                textBox1.Text += "6";
+            }
+        }
+
+        protected void Unnamed13_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "7";
+            }
+
+            else
+            {
+                textBox1.Text += "7";
+            }
+        }
+
+        protected void Unnamed14_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "8";
+            }
+
+            else
+            {
+                textBox1.Text += "8";
+            }
+        }
+
+        protected void Unnamed15_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "9";
+            }
+
+            else
+            {
+                textBox1.Text += "9";
+            }
+        }
+
+        protected void Unnamed17_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.IndexOf('-') < 0 && textBox1.Text != "0" && textBox1.Text != "")
+            {
+                textBox1.Text = textBox1.Text.Insert(0, "-");
+
+            }
+        }
+
+        protected void Unnamed18_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "0";
+            }
+
+            else
+            {
+                textBox1.Text += "0";
+            }
+        }
+
+        protected void Unnamed19_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                say2 = Convert.ToDouble(textBox1.Text);
+                say1 = Convert.ToDouble(num1.Text);
+                op = num2.Text;
+                switch (op)
+                {
+                    case "+":
+                        netice = say1 + say2;
+                        break;
+
+                    case "-":
+                        netice = say1 - say2;
+                        break;
+
+                    case "x":
+                        netice = say1 * say2;
+                        break;
+
+                    case "/":
+                        netice = say1 / say2;
+                        break;
+
+                }
+                textBox1.Text = netice.ToString();
+                textBox1.Text = "";
+                label1.Text = say1.ToString() + " " + op + " " + say2.ToString() + " " + "=" + " " + netice.ToString();
+
+            }
+        }
+      
+        protected void Unnamed12_Click(object sender, EventArgs e)
+        {
+            method("x");
+        }
+
+        protected void Unnamed8_Click(object sender, EventArgs e)
+        {
+            method("/");
+        }
+    }
+}
